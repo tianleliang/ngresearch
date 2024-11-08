aa27689dfd2447f01db7f88d93ebe403c5b13dfc
Amani Bobo <amanibobo1@gmail.com>
2024-11-08 15:29:36 -0500

    istg

--- src/app/(main)/app/layout.tsx
+++ src/app/(main)/app/layout.tsx
@@ -1,5 +1,3 @@
-import DashboardNavbar from "@/components/dashboard/dashboard-navbar";
-import DashboardSidebar from "@/components/dashboard/dashboard-sidebar";
 import React from 'react';
 
 interface Props {
@@ -9,9 +7,9 @@ interface Props {
 const DashboardLayout = ({ children }: Props) => {
     return (
         <div className="flex flex-col min-h-screen w-full">
-            <DashboardNavbar />
+            
             <main className="flex flex-col lg:flex-row flex-1 size-full">
-                <DashboardSidebar />
+               
                 <div className="w-full pt-14 lg:ml-72">
                     {children}
                 </div>
